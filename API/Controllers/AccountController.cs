0c2c571dd69213a727245a6fe123f4cc30c55f4d
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-11 16:05:38 +0300

    added login endpoint and dropped tabled

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,6 +37,26 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.AppUsers
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.AppUsers.AnyAsync(x => x.UserName == username.ToLower());
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
