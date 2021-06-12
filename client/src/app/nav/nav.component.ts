c9d645193be89cbb87e3c708b656655da2173a76
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-12 20:25:41 +0300

    persistent login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -12,7 +12,9 @@ export class NavComponent implements OnInit {
 
   constructor(private accountService: AccountService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
@@ -26,6 +28,16 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    },
+      error => {
+        console.log(error);
+      })
   }
 }
