8b565344f03c2136764525a8cffefab9c9a7640c
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-13 22:05:41 +0300

    parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,26 +12,16 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient,
+  constructor(
     private accountService: AccountService
   ) { }
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http
-      .get("https://localhost:5001/api/users")
-      .subscribe(
-        response => { this.users = response; },
-        error => { console.log(error); }
-      )
   }
 }
