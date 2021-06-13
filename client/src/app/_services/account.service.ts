1996e46323607e2d662be0de2019e2b34af89982
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-13 22:47:36 +0300

    hooking up the register method to the service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,6 +26,18 @@ export class AccountService {
     );
   }
 
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    )
+  }
+
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
