cb9b598ee7769efa86aaa17a755651755cc68ed2
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-14 16:32:04 +0300

    routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +13,10 @@ export class NavComponent implements OnInit {
   model: any = {};
   loggedInUser: string;
 
-  constructor(public accountService: AccountService) { }
+  constructor(
+    public accountService: AccountService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
 
@@ -24,7 +28,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
       window.location.reload();
     },
       error => {
@@ -35,5 +39,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
