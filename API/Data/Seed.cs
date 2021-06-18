fcd6d11aa23c9d88f4ce0e9c9cf1b3820cd32dce
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-18 11:15:57 +0300

    seed data and implementation created

---
+++ API/Data/Seed.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.AppUsers.AnyAsync()) return;
+
+            string SeedDataPath = "Data/UserSeedData.json";
+
+            var userData = await System.IO.File.ReadAllTextAsync(SeedDataPath);
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+
+                context.AppUsers.Add(user);
+            }
+        }
+    }
+}
