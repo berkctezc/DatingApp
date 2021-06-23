234c75d9056b037f5ee9e9a5a3f84cf9f4641872
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-23 19:18:07 +0300

    setting main photo in the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -36,4 +36,8 @@ export class MembersService {
       this.members[index] = member;
     }))
   }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {})
+  }
 }
