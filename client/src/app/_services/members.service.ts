5692a4fc34822fa283236391a712077f93aac452
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-23 19:37:43 +0300

    deleting photos

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -40,4 +40,8 @@ export class MembersService {
   setMainPhoto(photoId: number) {
     return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {})
   }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -45,6 +45,12 @@ export class PhotoEditorComponent implements OnInit {
     })
   }
 
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter(x => x.id !== photoId);
+    })
+  }
+
   initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + 'users/add-photo',
