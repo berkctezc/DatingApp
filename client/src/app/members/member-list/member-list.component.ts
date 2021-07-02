a3340ec78a40e4827fecb91151bd3afaa638a278
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-02 22:57:47 +0300

    readme updated to sec 13

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
-import { Observable } from 'rxjs';
 import { Pagination } from 'src/app/_models/pagination';
 import { UserParams } from 'src/app/_models/userParams';
-import { AccountService } from 'src/app/_services/account.service';
-import { take } from 'rxjs/operators';
 import { User } from 'src/app/_models/user';
 
 @Component({
