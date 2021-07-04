2b2cd8c577d49302c7ae94b3857770f3cbcf800e
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-04 20:30:51 +0300

    like functionality in client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -87,6 +87,14 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  getLikes(predicate: string) {
+    return this.http.get(this.baseUrl + 'likes?=' + predicate);
+  }
+
   private getPaginatedResult<T>(url, params) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
